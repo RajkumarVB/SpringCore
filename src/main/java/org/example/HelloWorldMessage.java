2aa46de026dbe794733e1d420df208adaa8d5b82
Rajkumar <rajkumarvb.career@gmail.com>
2023-10-14 16:27:39 +0530

    Circular dependency - Setter based DI will work

--- src/main/java/org/example/HelloWorldMessage.java
+++ src/main/java/org/example/HelloWorldMessage.java
@@ -1,10 +1,17 @@
 package org.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HelloWorldMessage {
+    private HelloWorldMessage helloWorldMessage;
     public String getMessage(){
+
         return "Hello World, Spring Core!";
+    }
+    @Autowired
+    public void setHelloWorldMessage(HelloWorldMessage helloWorldMessage) {
+        this.helloWorldMessage = helloWorldMessage;
     }
 }
