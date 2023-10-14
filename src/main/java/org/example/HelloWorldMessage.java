04d89478f5f5cee5cceed9bd0b8e63610d1f6295
Rajkumar <rajkumarvb.career@gmail.com>
2023-10-14 13:59:49 +0530

    1

--- src/main/java/org/example/HelloWorldMessage.java
+++ src/main/java/org/example/HelloWorldMessage.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class HelloWorldMessage {
     public String getMessage(){
         return "Hello World, Spring Core!";
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,16 @@
 package org.example;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan("org.example")
 public class Main {
     public static void main(String[] args) {
-        HelloWorldMessage helloWorldMessage = new HelloWorldMessage();
-        System.out.println(helloWorldMessage.getMessage());
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
+        HelloWorldMessage message = context.getBean(HelloWorldMessage.class);
+        System.out.println(message.getMessage());
+
     }
 }
